806e62ab910c8d0e4d4826694a1d7799f301d482
Nitin Ramadoss <nitinramadoss@gmail.com>
2020-10-17 10:50:44 -0400

    Optimizes camera for handtrack.js

--- src/app.js
+++ src/app.js
@@ -1,47 +1,66 @@
 const modelParams = {
-    flipHorizontal: true,   
-    imageScaleFactor: 0.7, 
-    maxNumBoxes: 6,        
-    iouThreshold: 0.5,      
-    scoreThreshold: 0.80,    
-  }
-  
+    flipHorizontal: true,  
+    maxNumBoxes: 20,      
+    iouThreshold: 0.5,     
+    scoreThreshold: 0.6,    
+}
 
-navigator.getUserMedia = 
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
-
-const video = document.querySelector('#video');
-const canvas = document.querySelector('#canvas');
-const context = canvas.getContext('2d');
-let model;
+let isVideo = false;
+let model = null;
 
 
 
-handTrack.startVideo(video).then(status => {
-    if(status){
-        navigator.getUserMedia(
-            {video: {}}, 
-            stream => {
-                video.srcObject = stream;
-                setInterval(runDetection, 1000)
-            },
-            err => console.log(err)
-        );
+const video = document.getElementById("myvideo");
+const canvas = document.getElementById("canvas");
+let trackButton = document.getElementById("trackbutton");
+const context = canvas.getContext("2d");
+
+
+function toggleVideo() {
+    if (!isVideo) {
+        console.log("Starting video");
+        startVideo();
+    } else {       
+        handTrack.stopVideo(video)
+        isVideo = false;
     }
-});
+}
 
-function runDetection(){
-    model.detect(video)
-        .then(predictions => {
-            console.log(predictions);
-            model.renderPredictions(predictions, canvas, context, video);
-        });
+function startVideo() {
+    handTrack.startVideo(video).then(function (status) {
+        console.log("Initiating camera", status);
+        if (status) {
+            isVideo = true
+            runDetection()
+        } else {
+            console.log("Camera failed to start");
+        }
+    });
+}
+
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, video);
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+    });
+}
+
+function runDetectionImage(img) {
+    model.detect(img).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, img);
+    });
 }
 
 
 handTrack.load(modelParams).then(lmodel => {
     model = lmodel;
+});
+
+trackButton.addEventListener("click", function(){
+    toggleVideo();
 });
