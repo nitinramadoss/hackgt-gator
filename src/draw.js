641cd973d36225f40623ba8113d7467610fdd493
Raghu Radhakrishnan <rradhakrishnan1@ufl.edu>
2020-10-17 08:18:01 -0400

    increase threshold and fix modules

--- src/draw.js
+++ src/draw.js
@@ -1,9 +1,32 @@
 function draw(options) {
     let canvas = document.getElementById('canvas');
     const context = canvas.getContext("2d");
-    if (canvas.getContext) {
-      context.strokeRect(options[0], options[1], options[2] - 50, options[3] - 50);
+    
+    if(options.type === 'rectangle') {
+        drawRectangle(options, context)
+    } else if(options.type === 'text') {
+        drawText(options, context)
     }
 }
+
+function drawRectangle(options, context) {
+    let coords = options.bbox
+    if (canvas.getContext) {
+      context.fillRect(coords[0], coords[1], coords[2], coords[3]);
+    }
+}
+
+function drawText(options, context) {
+    let coords = options.bbox    
+    let text = document.getElementById("inputBox").value;
+    context.font = '48px serif';
+
+    if (text != undefined) {
+        context.fillText(text, coords[0], coords[1] + 0.5*coords[3]);
+    } else {
+        context.fillText("", coords[0], coords[1]);
+    }
+  }
+ 
 
 export default draw
