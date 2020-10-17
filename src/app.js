be53cab60b144e905645445b0e7caeeb65ebc282
Raghu Radhakrishnan <rradhakrishnan1@ufl.edu>
2020-10-17 04:03:24 -0400

    amazing commit message

--- src/app.js
+++ src/app.js
@@ -43,11 +43,25 @@ function runDetection() {
     model.detect(video).then(predictions => {
         console.log("Predictions: ", predictions);
         model.renderPredictions(predictions, canvas, context, video);
+        
         if (isVideo) {
-            requestAnimationFrame(runDetection);
+            requestAnimationFrame(runDetection);     
         }
+
+        if(predictions !== undefined)
+            draw(predictions[0].bbox)
     });
 }
+
+function draw(coords) {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+  
+      context.clearRect(45, 45, 60, 60);
+      context.strokeRect(coords[0], coords[1], coords[2] - 50, coords[3] - 50);
+    }
+}
+ 
 
 function runDetectionImage(img) {
     model.detect(img).then(predictions => {
